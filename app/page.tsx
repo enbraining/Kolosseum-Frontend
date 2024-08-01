d1746a0562fed5457fc8c84761cd6bb690210608
enbraining <me@fodo.dev>
2024-08-01 22:43:11 +0900

    :recycle: 선택한 년도 커진 사이즈 유지

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,7 @@ export default function Home() {
         <div className="absolute w-full overflow-x-scroll scrollbar-hide">
             <div className="mt-10 mx-auto text-stone-500 [writing-mode:vertical-lr] px-10">
             {years.map(value =>
-                <div onClick={() => {setYear(findYear(value))}} key={value} className="text-2xl font-bold hover:font-black hover:text-4xl hover:text-stone-700 duration-150">{value}</div>)
+                <div onClick={() => {setYear(findYear(value))}} key={value} className={`${value == year?.year ? "text-4xl text-stone-700 font-black" : "text-2xl"} font-bold hover:font-black hover:text-4xl hover:text-stone-700 duration-150`}>{value}</div>)
             }
             </div>
         </div>
@@ -28,6 +28,7 @@ export default function Home() {
             <div className={'mb-5'}>    <h1 className="text-3xl text-red-950 font-bold">안녕하세요, 역사 기록 프로젝트</h1>
             <h1 className="text-3xl text-red-950 font-bold">Kolosseum입니다.</h1></div>
             <p className="text-xl font-medium">현재는 데이터가 정확하지 않을 수도 있으며 이슈 혹은 메일을 보내주시면 빠르게 추가하거나 수정하겠습니다.</p>
+            <p className="text-xl font-medium">me@fodo.dev</p>
                 </div>}
         </div>
     </main>
