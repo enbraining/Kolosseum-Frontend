83503c1e2d6b59a8de6e1f557c04eb3ce57e33c9
enbraining <me@fodo.dev>
2024-08-08 17:07:48 +0900

    :recycle: 회원가입 axios 수정

--- app/actions.ts
+++ app/actions.ts
@@ -1,13 +1,13 @@
-'use server'
+'use server';
 
-import { AxiosFetch } from "./utils/axios"
+import axios from 'axios';
 
 export async function createUser(event: FormData) {
-    const email = event.get('email')?.toString()!
-    const password = event.get('password')?.toString()!
+  const email = event.get('email')?.toString()!;
+  const password = event.get('password')?.toString()!;
 
-    const response = await AxiosFetch.post('/auth/join', {
-        "email": email,
-        "password": password
-    })
+  await axios.post('http://localhost:8080/auth/join', {
+    email: email,
+    password: password,
+  });
 }
