4f44f1ed30a0799bc4f80d8bc67152eee37b09f7
enbraining <me@fodo.dev>
2024-08-09 19:20:44 +0900

    :recycle: footer 정보 수정

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,18 +1,18 @@
-"use client"
+'use client';
 
-import Link from "next/link";
-import { StyledFooter } from "../styled/Tag";
-import { CarbonLogoDiscord, GrommetIconsGithub } from "./Icons";
+import { StyledFooter } from '../styled/Tag';
+import { H1 } from '../styled/Text';
 
-export default function Footer(){
-    return (
-        <StyledFooter className="border-t mt-10 grid pt-4 mx-10">
-            <div className="ml-auto gap-x-2 flex">
-                <Link className="text-teal-600 hover:text-teal-800" href={"https://github.com/enbraining"}><GrommetIconsGithub scope={30} /></Link>
-                <Link className="text-teal-600 hover:text-teal-800" href={"https://discord.gg/36tCw3kYuJ"}>
-                    <CarbonLogoDiscord scope={30} />
-                </Link>
-            </div>
-        </StyledFooter>
-    )
+export default function Footer() {
+  return (
+    <StyledFooter className="border-t grid pt-6 m-10 gap-y-3">
+      <div>
+        <H1>Kolosseum</H1>
+        <p className="text-neutral-600">역사를 잊은 민족에게 미래는 없다.</p>
+      </div>
+      <div>
+        <p className="text-neutral-600">개발자 김동학 | me@fodo.dev</p>
+      </div>
+    </StyledFooter>
+  );
 }
