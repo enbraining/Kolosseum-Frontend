53b9e33eab653ec8187f2b609c8a8610fe16b3d6
enbraining <me@fodo.dev>
2024-08-11 10:50:40 +0900

    :recycle: 색상 수정

--- app/service/styles/Text.tsx
+++ app/service/styles/Text.tsx
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const H1 = styled.h1`
-  color: rgb(69, 10, 10);
+  color: var(--primary-color);
   font-size: 1.5rem;
   line-height: 2rem;
   font-weight: 600;
